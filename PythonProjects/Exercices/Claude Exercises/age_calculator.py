28dcc1875bd49628aebe9331f4efcdbc0ead8732
lobkevl <lobke.vanlangendonck@gmail.com>
2026-03-05 22:33:49 +0200

    Add extra condition for a baby

--- PythonProjects/Exercices/Claude Exercises/age_calculator.py
+++ PythonProjects/Exercices/Claude Exercises/age_calculator.py
@@ -2,8 +2,10 @@ birth_year = int(input("Enter your birth year: "))
 current_year = 2026
 age = current_year - birth_year
 
-if age < 13:
-    print("You are a baby or child! 👶")
+if age < 2:
+    print("You are a baby! 👶")
+elif age < 13:
+    print("You are a child! 👶")
 elif age < 18:
     print("You are a teenager! 🧑")
 else:
