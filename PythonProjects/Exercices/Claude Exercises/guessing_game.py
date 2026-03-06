9eaa52ddc9a04ced972e5d968ac619145dd615e0
lobkevl <lobke.vanlangendonck@gmail.com>
2026-03-06 23:01:50 +0200

    Guessing the secret number

---
+++ PythonProjects/Exercices/Claude Exercises/guessing_game.py
@@ -0,0 +1,15 @@
+import random
+secret = random.randint(1,10)
+attempts = 0
+guess = int(input("Guess the secret number :"))
+
+while guess != secret :
+    if guess > secret :
+        print("Too high")
+    if guess < secret :
+        print("Too low")
+    attempts = attempts + 1
+    guess = int(input("Guess again :"))
+
+attempts = attempts + 1
+print(f"You guessed the secret number after {attempts} attempts")
